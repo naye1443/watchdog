99c39d4b9340d0eb129576292e37e292bbd598a0
Austin McCallister <austin.mccallister@protonmail.com>
2022-07-14 09:27:43 -0400

    Update path to graphql.js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 const express = require('express');
-const main = require('../graphql.js');
+const main = require('../graphql/graphql.js');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   main().then((obj) => {
-    for (const [key, value] of Object.entries(obj)) {
-      console.log(value.name);
-      console.log(value.status);
-    }
     res.render('index', { title: 'Status', db: obj });
   });
 });
