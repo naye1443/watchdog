79afbf488a27e9e5af397cee06954acf1b1e4bce
Austin McCallister <austin.mccallister@protonmail.com>
2022-07-14 18:47:50 -0400

    Optimize query execution time

--- graphql/graphql.js
+++ graphql/graphql.js
@@ -2,7 +2,7 @@ const { GraphQLClient, gql } = require('graphql-request');
 const fs = require('fs');
 const path = require('path');
 
-const main = async () => {
+const query = async (guid) => {
   const endpoint = 'https://api.newrelic.com/graphql';
 
   const graphQLClient = new GraphQLClient(endpoint, {
@@ -11,32 +11,40 @@ const main = async () => {
     },
   });
 
-  const guidList = JSON.parse(fs.readFileSync(path.resolve(__dirname, './guidlist.json'), 'utf-8'));
-  const entity = {};
-  for (const [key, value] of Object.entries(guidList)) {
-    const query = gql`
-      {
-        actor {
-          entity(guid: "${value.guid}") {
-            alertSeverity
-            name
-            reporting
-            entityType
-            accountId
-          }
+  const schema = gql`
+    {
+      actor {
+        entity(guid: "${guid}") {
+          alertSeverity
+          name
+          guid
         }
       }
-    `
-    const data = await graphQLClient.request(query);
-    entity[key] = {
-      guid: value.guid,
-      name: value.name,
-      status: data.actor.entity.alertSeverity,
-    };
+    }
+  `
+
+  return graphQLClient.request(schema);
+}
+
+const output = async () => {
+  const guidList = JSON.parse(fs.readFileSync(path.resolve(__dirname, './guidlist.json'), 'utf-8'));
+  const promises = [];
+  const entity = {};
+
+  for (const [key, value] of Object.entries(guidList)) {
+    promises.push(query(value.guid));
   }
+
+  await Promise.all(promises).then((values) => {
+    for (const [key, value] of Object.entries(values)) {
+      entity[key] = {
+        guid: value.actor.entity.guid,
+        name: guidList[key].name,
+        status: value.actor.entity.alertSeverity,
+      };
+    }
+  });
   return entity;
 }
 
-main().catch((error) => console.error(error));
-
-module.exports = main;
+module.exports = output;
